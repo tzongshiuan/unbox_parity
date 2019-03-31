b9dda1aabb93ddb31e023dbd75c41f95538d2f87
tsunghsuanlai <tzongshiuan@gmail.com>
2019-03-31 20:50:46 +0800

    [What] 1. Finish the first version of search page UI.

--- app/src/main/java/com/hsuanparty/unbox_parity/view/ui/search/SearchFragment.kt
+++ app/src/main/java/com/hsuanparty/unbox_parity/view/ui/search/SearchFragment.kt
@@ -1,12 +1,20 @@
 package com.hsuanparty.unbox_parity.view.ui.search
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.input.InputManager
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 
 import com.hsuanparty.unbox_parity.databinding.SearchFragmentBinding
 import com.hsuanparty.unbox_parity.di.Injectable
@@ -69,16 +77,36 @@ class SearchFragment : Fragment(), Injectable {
     }
 
     private fun initUI() {
+        mBinding.searchEditText.setOnEditorActionListener(object: TextView.OnEditorActionListener {
+            override fun onEditorAction(view: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    hideKeyboard()
+                    performSearch()
+                    return true
+                }
+                return false
+            }
+        })
+
         mBinding.searchBtn.setOnClickListener {
-            mBinding.searchEditText.setText("1212")
+            performSearch()
         }
 
         mBinding.speakBtn.setOnClickListener {
-            mBinding.searchEditText.setText("5566")
+            // TODO voice record and then search
         }
 
         mBinding.segmentView.setOnSelectionChangedListener { identifier, value ->
             LogMessage.D(TAG, "identifier: $identifier, value: $value")
         }
+    }
+
+    private fun hideKeyboard() {
+        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+    }
+
+    private fun performSearch() {
+        viewModel.search(mBinding.searchEditText.text.toString())
     }
 }
--- app/src/main/java/com/hsuanparty/unbox_parity/view/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/hsuanparty/unbox_parity/view/ui/search/SearchViewModel.kt
@@ -2,11 +2,16 @@ package com.hsuanparty.unbox_parity.view.ui.search
 
 import androidx.lifecycle.ViewModel;
 import com.hsuanparty.unbox_parity.di.Injectable
+import com.hsuanparty.unbox_parity.utils.LogMessage
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor() : ViewModel(), Injectable {
 
     companion object {
         private val TAG = SearchViewModel::class.java.simpleName
+    }
+
+    fun search(str: String) {
+        LogMessage.D(TAG, "start search with string: $str")
     }
 }
